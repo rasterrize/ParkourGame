7cee9f9b1671b7721262ef7ccca1cc8d949966fa
rasterrize <jyestar343@gmail.com>
2024-10-29 17:55:45 +1100

    Use new isGrounded technique for player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -20,6 +20,11 @@ namespace Player
         private const float DEFAULT_GRAVITY = -9.81f;
 
         [SerializeField] private Camera playerCamera;
+        [SerializeField] private GameObject groundCheckObject;
+        [SerializeField] private float groundCheckRadius = 0.4f;
+        [SerializeField] private LayerMask groundLayer;
+        
+        private bool isGrounded;
         private CharacterController controller; // the character controller of the player
 
         [Tooltip("The gravity of the player. This defaults to earth's gravity.")] 
@@ -39,6 +44,9 @@ namespace Player
         
         [Tooltip("The maximum speed the player can slide. Usually only reached when sliding downhill.")]
         [SerializeField] private float maxSlideSpeed;
+
+        [Tooltip("The distance to move the camera down on the y axis when the player slides.")]
+        [SerializeField] private float slideCameraYDistance;
         
         private float currentSlidePenalty;
         private float currentSlideVelocity;
@@ -47,8 +55,6 @@ namespace Player
         private Vector2 moveInputValues;
 
         private MovementType currentMovementState;
-    
-        //private bool allowMovement = true;
 
         private void Start()
         {
@@ -57,13 +63,20 @@ namespace Player
 
         private void Update()
         {
-            // if (!allowMovement)
-            //     return;
+            velocity.y -= gravity * -2f * Time.deltaTime;
+            isGrounded = Physics.CheckSphere(groundCheckObject.transform.position, groundCheckRadius, groundLayer);
+            
+            if (isGrounded && velocity.y < 0)
+            {
+                // Force the player to the ground
+                velocity.y = -2f;
+            }
         
-            // Check if the player is trying to move
+            // Check if the player is trying to move or not
             if (moveInputValues == Vector2.zero)
                 currentMovementState = MovementType.Idle;
-
+            
+            // Update based on movement state
             switch (currentMovementState)
             {
                 case MovementType.Idle:
@@ -89,16 +102,17 @@ namespace Player
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-        
-            // apply gravity to character controller
-            velocity.y -= controller.isGrounded ? -1f : gravity * -2f * Time.deltaTime;
+            
+            // Move player using gravity
             controller.Move(velocity * Time.deltaTime);
         }
 
         private void Move(Vector2 inputs)
         {
+            // Check if the player is holding sprint
             currentMovementState =  isHoldingSprintKey ? MovementType.Running : MovementType.Walking;
         
+            // Move the player in the direction they are facing
             var moveVector = controller.transform.forward * inputs.y + controller.transform.right * inputs.x;
             var currentSpeed = isHoldingSprintKey ? runSpeed : walkSpeed;
             controller.Move(currentSpeed * Time.deltaTime * moveVector);
@@ -119,7 +133,7 @@ namespace Player
             currentSlideVelocity = slideStartSpeed;
                 
             // Move camera down to emulate character sliding
-            playerCamera.transform.localPosition = new Vector3(playerCamera.transform.localPosition.x, playerCamera.transform.localPosition.y - 0.5f, playerCamera.transform.localPosition.z);
+            playerCamera.transform.localPosition = new Vector3(0, playerCamera.transform.localPosition.y - slideCameraYDistance, 0);
             
             // Shrink player collider
             controller.center = new Vector3(0, -0.5f, 0);
@@ -129,7 +143,6 @@ namespace Player
         private void EndSlide()
         {
             currentSlideVelocity = 0.0f;
-        
             currentMovementState = MovementType.Idle;
         
             // Reset camera
@@ -165,8 +178,11 @@ namespace Player
 
         public void OnJumpAction(InputAction.CallbackContext context)
         {
+            if (!context.performed)
+                return;
+            
             // Check if in correct state to jump
-            if (currentMovementState is MovementType.Idle or MovementType.Walking or MovementType.Running or MovementType.Sliding && controller.isGrounded)
+            if (currentMovementState is MovementType.Idle or MovementType.Walking or MovementType.Running or MovementType.Sliding && isGrounded)
             {
                 Jump();
             }
@@ -190,7 +206,7 @@ namespace Player
                     EndSlide();
             }
             
-            if (context.started && controller.isGrounded && currentMovementState == MovementType.Running)
+            if (context.started && isGrounded && currentMovementState == MovementType.Running)
             {
                 BeginSlide();
             }
